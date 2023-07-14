c1ad00e630d47f941bf086f85c838890c64107ed
Franco Brattoli <fbratt29@gmail.com>
2023-07-14 12:48:47 -0300

    modifiamos script1 generar.sh

--- scripts/generar.sh
+++ scripts/generar.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-# Crear el directorio de almacenamiento si no existe
+# Creamos un directorio para almacenar las imagenes generadas 
 mkdir -p almacenamiento
 
-# Descargar el archivo de nombres de personas
+# Descargamos el csv con la lista aleatoria de nombres de personas
 wget -O dict.csv https://raw.githubusercontent.com/fernandezpablo85/name_suggestions/master/assets/dict.csv
 
-# Obtener el número de imágenes a generar del primer argumento
+# Obtenenemos la cantidad de imágenes a generar mediante el primer argumento
 num_images=$1
 
-# Función para generar un nombre de archivo aleatorio de la lista de nombres
+# Mediante esta función generamos nombres aleatorios obtenidos de la lista del csv, para posteriormente asignarselo como nombre
+# de archivo a la imagen generada
 generate_random_filename() {
   local random_line=$(( RANDOM % 96222 + 1 ))
   local random_name=$(sed -n ${random_line}p dict.csv)
   echo "$random_name.jpg"
 }
 
-# Descargar las imágenes y asignar nombres de archivo al azar
+# Descargamos las imágenes y asignamos nombres de archivo al azar
 for ((i=0; i<num_images; i++)); do
   random_filename=$(generate_random_filename)
   wget -O "almacenamiento/$random_filename" "https://source.unsplash.com/random/900x700/?person"
-  sleep 1  # Esperar 1 segundo entre descargas para evitar problemas
+  sleep 2  # Esperamos 2 segundos entre descargas para evitar problemas
 done
 
-# Comprimir las imágenes en un archivo ZIP
+# Comprimimos las imágnenes mediante zip. (Evaluar utilizar tar)
 zip -r imagenes.zip almacenamiento
 
-# Generar el archivo con la suma de verificación (checksum) del ZIP
+# Generamos el archivo con la suma de verificación (checksum) del ZIP
 md5sum imagenes.zip > checksum.txt
