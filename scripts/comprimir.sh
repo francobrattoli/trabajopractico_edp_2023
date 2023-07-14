23534d31f8f5c0ca924dc33e1c30b5fcb14d52c3
Franco Brattoli <fbratt29@gmail.com>
2023-07-14 12:59:48 -0300

    modificamos script comprimir.sh

--- scripts/comprimir.sh
+++ scripts/comprimir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Carpeta de entrada con las imágenes procesadas
+# asigamos a una variable la carpeta con las imagenes procesadas
 directorio_procesadas="imagenes_procesadas"
 
 # Archivo con la lista de nombres de todas las imágenes
@@ -15,17 +15,16 @@ archivo_total_a="total_a.txt"
 # Archivo comprimido que incluirá los archivos generados y las imágenes
 archivo_comprimido="imagenes_comprimidas.zip"
 
-# Generar archivo con la lista de nombres de todas las imágenes
+# Generamos archivo con la lista de nombres de todas las imágenes
 ls "$directorio_procesadas"/*.jpg > "$archivo_lista_todas"
 
-# Generar archivo con la lista de nombres válidos
+# Generamos archivo con la lista de nombres válidos
 grep -E '^[A-Z][a-z]+\.jpg$' "$archivo_lista_todas" > "$archivo_lista_validas"
 
-# Generar archivo con el total de personas cuyo nombre finaliza con la letra 'a'
+# Generamos archivo con el total de personas cuyo nombre finaliza con la letra 'a'
 grep -iE '[A-Z][a-z]+a\.jpg$' "$archivo_lista_validas" | wc -l > "$archivo_total_a"
 
-# Comprimir los archivos generados y las imágenes en un archivo comprimido
+# Comprimimos los archivos generados y las imágenes en un archivo comprimido
 zip "$archivo_comprimido" "$archivo_lista_todas" "$archivo_lista_validas" "$archivo_total_a" "$directorio_procesadas"/*.jpg
 
-echo "Proceso de compresión de archivos y generación del archivo comprimido completado."
 
